5d33724fae187726a7d0c20034353d72a6761b25
Paul Normington <normyp1@yahoo.co.uk>
2016-01-10 18:29:52 +0000

    Back to normal

---
+++ Assets/Scripts/Proj.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Proj : MonoBehaviour {
+
+	float t;
+	// Use this for initalization
+	void Awake() 
+	{
+		this.GetComponent<Rigidbody2D>().AddForce (new Vector2 (250, 0));
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		t += Time.deltaTime;
+		if (t >= 0.5) {
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+		//OnButtonPress instantiate new player at scale 0.5, 0.5, 0.5.
+		//Then move it forward about 10 in the direction aiming.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			Instantiate(bullet, new Vector3(0,0,0), Quaternion.identity);
+		}
+	}
+}
