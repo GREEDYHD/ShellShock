ed9df0bbf0b6a7648c7b7393b8c716f8d85b7145
David Greedy <davidgreedy1995@gmail.com>
2016-01-10 17:24:44 +0000

    Movement script added along with temporary player placeholder

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+	public float mMovementSpeed;
+
+	Rigidbody2D mRigidBody2D;
+
+	void Start ()
+	{
+		mRigidBody2D = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.W)) {
+			mRigidBody2D.AddForce (new Vector2 (0, mMovementSpeed));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			mRigidBody2D.AddForce (new Vector2 (-mMovementSpeed, 0));
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			mRigidBody2D.AddForce (new Vector2 (0, -mMovementSpeed));
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			mRigidBody2D.AddForce (new Vector2 (mMovementSpeed, 0));
+		}
+	}
+}
