1efca3a2cb0430d2f777e3e767b969ac2fc94091
SilentSupernova <chrisyoules93@gmail.com>
2016-02-17 18:28:58 +0000

    Re did the DisplayAmmoCount.cs from 14 lines to 1. Works natively with Weapon Switching.
    Signed-off-by: SilentSupernova <chrisyoules93@gmail.com>

--- Assets/Scripts/DisplayAmmoCount.cs
+++ Assets/Scripts/DisplayAmmoCount.cs
@@ -5,17 +5,7 @@ using System.Collections;
 
 public class DisplayAmmoCount : MonoBehaviour
 {
-	public Pistol pistol;
-	public AssaultRifle assualtRifle;
-    public Shotgun shotgun;
-	public Sniper sniper;
-    public MiniGun minigun;
-    public RocketLauncher rocketLauncher;
-	public GrenadeLauncher grenadeLauncher;
-
     public Text ammoCount;
-
-    //going to need some way of telling each others ammo counts apart.
     public int playerID;
 
     public Player player;
@@ -31,35 +21,7 @@ public class DisplayAmmoCount : MonoBehaviour
 
     void Update()
     {
-        //change the ammo count depending on which weapon the player has equipped.
-        if (player.mEquippedWeapon == shotgun)
-        {
-            ammoCount.text = shotgun.mAmmoRemaining.ToString();
-        }
-            else if (player.mEquippedWeapon == minigun)
-            {
-                ammoCount.text = minigun.mAmmoRemaining.ToString();
-            }
-                else if (player.mEquippedWeapon == rocketLauncher)
-                {
-                    ammoCount.text = rocketLauncher.mAmmoRemaining.ToString();
-                }
-					else if (player.mEquippedWeapon == pistol)
-					{
-						ammoCount.text = pistol.mAmmoRemaining.ToString();
-					}
-						else if (player.mEquippedWeapon == assualtRifle)
-							{
-								ammoCount.text = assualtRifle.mAmmoRemaining.ToString();
-							}
-							else if (player.mEquippedWeapon == sniper)
-								{
-									ammoCount.text = sniper.mAmmoRemaining.ToString();
-								}
-								else if (player.mEquippedWeapon == grenadeLauncher)
-									{
-										ammoCount.text = grenadeLauncher.mAmmoRemaining.ToString();
-									}
+		ammoCount.text = player.mEquippedWeapon.mAmmoRemaining.ToString ();
     }
 
 }
